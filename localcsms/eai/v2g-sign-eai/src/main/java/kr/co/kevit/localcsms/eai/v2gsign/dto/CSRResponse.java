c22edbe823e63946e7a91d3d4e5bacd99d73ddf7
skel-charles <charles@skelectlink.com>
2026-09-16 00:51:08 +0900

    사용하지 않는 컴포넌트 삭제.

--- localcsms/eai/v2g-sign-eai/src/main/java/kr/co/kevit/localcsms/eai/v2gsign/dto/CSRResponse.java
+++
@@ -1,9 +0,0 @@
-package kr.co.kevit.localcsms.eai.v2gsign.dto;
-
-public class CSRResponse {
-    private String result;
-
-    public CSRResponse(String result) { this.result = result; }
-
-    public String getResult() { return result; }
-}
